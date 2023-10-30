b85ac9f63d6c8a50118d35f78da7d144457ec26d
Vishal Garje <68316341+vishal4068@users.noreply.github.com>
2023-10-30 13:13:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+//like
+function likeone() {
+    if ((document.getElementById("heartone").name) == "heart-outline") {
+        document.getElementById("heartone").name = "heart";
+        document.getElementById("heartone").style.color = "red";
+    }
+    else {
+        document.getElementById("heartone").name = "heart-outline";
+        document.getElementById("heartone").style.color = "black";
+    }
+}
+
+
+function liketwo() {
+    if ((document.getElementById("hearttwo").name) == "heart-outline") {
+        document.getElementById("hearttwo").name = "heart";
+        document.getElementById("hearttwo").style.color = "red";
+    }
+    else {
+        document.getElementById("hearttwo").name = "heart-outline";
+        document.getElementById("hearttwo").style.color = "black";
+    }
+}
+
+
+function likethree() {
+    if ((document.getElementById("heartthree").name) == "heart-outline") {
+        document.getElementById("heartthree").name = "heart";
+        document.getElementById("heartthree").style.color = "red";
+    }
+    else {
+        document.getElementById("heartthree").name = "heart-outline";
+        document.getElementById("heartthree").style.color = "black";
+    }
+}
+
+function likefour() {
+    if ((document.getElementById("heartfour").name) == "heart-outline") {
+        document.getElementById("heartfour").name = "heart";
+        document.getElementById("heartfour").style.color = "red";
+    }
+    else {
+        document.getElementById("heartfour").name = "heart-outline";
+        document.getElementById("heartfour").style.color = "black";
+    }
+}
+
+//dblclicklike
+function likeonedbl() {
+    if ((document.getElementById("heartone").name) == "heart-outline") {
+        document.getElementById("heartone").name = "heart";
+        document.getElementById("heartone").style.color = "red";
+    }
+}
+
+
+function liketwodbl() {
+    if ((document.getElementById("hearttwo").name) == "heart-outline") {
+        document.getElementById("hearttwo").name = "heart";
+        document.getElementById("hearttwo").style.color = "red";
+    }
+}
+
+
+function likethreedbl() {
+    if ((document.getElementById("heartthree").name) == "heart-outline") {
+        document.getElementById("heartthree").name = "heart";
+        document.getElementById("heartthree").style.color = "red";
+    }
+}
+
+function likefourdbl() {
+    if ((document.getElementById("heartfour").name) == "heart-outline") {
+        document.getElementById("heartfour").name = "heart";
+        document.getElementById("heartfour").style.color = "red";
+    }
+}
+
+//savepost
+function saveone() {
+    if ((document.getElementById("saveone").name) == "bookmark-outline") {
+        document.getElementById("saveone").name = "bookmark";
+        document.getElementById("saveone").style.color = "black";
+    }
+    else {
+        document.getElementById("saveone").name = "bookmark-outline";
+        document.getElementById("saveone").style.color = "black";
+    }
+}
+
+function savetwo() {
+    if ((document.getElementById("savetwo").name) == "bookmark-outline") {
+        document.getElementById("savetwo").name = "bookmark";
+        document.getElementById("savetwo").style.color = "black";
+    }
+    else {
+        document.getElementById("savetwo").name = "bookmark-outline";
+        document.getElementById("savetwo").style.color = "black";
+    }
+}
+
+function savethree() {
+    if ((document.getElementById("savethree").name) == "bookmark-outline") {
+        document.getElementById("savethree").name = "bookmark";
+        document.getElementById("savethree").style.color = "black";
+    }
+    else {
+        document.getElementById("savethree").name = "bookmark-outline";
+        document.getElementById("savethree").style.color = "black";
+    }
+}
+
+function savefour() {
+    if ((document.getElementById("savefour").name) == "bookmark-outline") {
+        document.getElementById("savefour").name = "bookmark";
+        document.getElementById("savefour").style.color = "black";
+    }
+    else {
+        document.getElementById("savefour").name = "bookmark-outline";
+        document.getElementById("savefour").style.color = "black";
+    }
+}
